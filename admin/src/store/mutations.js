453b1a00beb581262aa0cba0a717bf790db76b1c
huanghuiming <1123082535@qq.com>
2018-11-07 15:42:15 +0800

    vue状态管理设置

---
+++ admin/src/store/mutations.js
@@ -0,0 +1,10 @@
+const mutations = {
+//  全局变量
+  SET_TOKEN:(state,token)=>{
+    state.token = token
+  },
+  SET_USERNAME:(state,name)=>{
+    state.username = username
+}
+}
+export default mutations
